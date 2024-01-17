a6d6152c6985bc869a880898a80dec5af43ec331
Shubham Jagatap <sjagatap@salesforce.com>
2024-01-18 01:04:23 +0530

    Add middleware and function to handel Errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ app.listen(process.env.PORT, ()=>{
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+
+
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false,
+        message,
+        statusCode,
+    })
+})
