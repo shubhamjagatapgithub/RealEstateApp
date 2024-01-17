91f44a681e6ef0154ae14fc7d4789deb3289b6a7
Shubham Jagatap <sjagatap@salesforce.com>
2024-01-18 00:50:29 +0530

    Create Signup Api Route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 //Create Express App
 const app = express();
 
+app.use(express.json());
+
 //Listen to port 3000
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}!`);
@@ -22,4 +25,5 @@ app.listen(process.env.PORT, ()=>{
 
 //Route to routes
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
